20967476eaa3458fffe16fa35728162049cabc07
skanjarla <skanjarla@zyclyx.com>
2020-10-19 20:35:55 +0530

    Add send email on content creation functionality

--- api/business-enquiries/controllers/business-enquiries.js
+++ api/business-enquiries/controllers/business-enquiries.js
@@ -1,8 +1,46 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    let name = "business-enquiries";
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      /* eslint-disable */
+      entity = await strapi.services[name].create(data, { files });
+    } else {
+      entity = await strapi.services[name].create(ctx.request.body);
+    }
+    console.log("Send email notification", entity);
+    await strapi.plugins["email"].services.email.send({
+      to: "skanjarla@zyclyx.com",
+      from: "skanjarla@zyclyx.com",
+      cc: "skanjarla@zyclyx.com",
+      subject: "business enquiry from - website",
+      text: `
+        Dear Team,
+
+        Hope you are doing well.
+        
+        Please find the enquiry details below from ${entity.Website}.
+        
+         Full Name : ${entity.Full_Name}
+
+         Phone : ${entity.Phone}
+
+         Email : ${entity.Email}
+
+         Message: ${entity.Message}
+        
+        Thanks & Regards,
+           
+        `,
+    });
+    return sanitizeEntity(entity, { model: strapi.models[name] });
+  },
+};
--- api/payslips/controllers/payslips.js
+++ api/payslips/controllers/payslips.js
@@ -1,8 +1,38 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.payslips.create(data, { files });
+    } else {
+      entity = await strapi.services.payslips.create(ctx.request.body);
+    }
+    console.log("Send email notification", entity);
+    await strapi.plugins["email"].services.email.send({
+      to: "skanjarla@zyclyx.com",
+      from: "skanjarla@zyclyx.com",
+      cc: "srikanthkanjarla@gmail.com",
+      subject: `Payslip ${entity.Month}-${entity.Year}`,
+      text: `
+        Dear ${entity.Employee_Name},
+
+        Hope you are doing well.
+        
+        Please find the attached payslip for the month of  ${entity.Month}-${entity.Year}.
+        
+        If you have any questions, do not hesitate to contact HR.
+        
+        Thanks & Regards,
+        HR Operations  
+        `,
+    });
+    return sanitizeEntity(entity, { model: strapi.models.payslips });
+  },
+};
