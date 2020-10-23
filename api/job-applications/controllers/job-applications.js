e01a1a151862b446cb2850c02c2478f0a1d92f72
skanjarla <skanjarla@zyclyx.com>
2020-10-23 18:24:05 +0530

    Add Email notifications
    Trigger email notifications on application form submission

--- api/job-applications/controllers/job-applications.js
+++ api/job-applications/controllers/job-applications.js
@@ -1,8 +1,51 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    const controllerName = "job-applications";
+    // query email to and cc list from emails-lists collection for careers_form
+    const emailList = await strapi
+      .query("email-lists")
+      .find({ List_Name: "careers_form" });
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services[controllerName].create(data, { files });
+      await strapi.plugins["email"].services.email.send({
+        to: emailList[0].To_List,
+        from: "skanjarla@zyclyx.com",
+        cc: emailList[0].CC_List,
+        replyTo: "hr.operations@zyclyx.com",
+        subject: `Job Applicaton - ${data.Position}`,
+        html: `
+            <p>Dear Team,</p>
+
+            <p>Please find the applicant details and attached resume below for <strong>${data.Position}</strong> position from ${data.Website} website.</p>
+            
+            <p>Fullname - ${data.Firstname} ${data.Lastname}</p>         
+            <p>Email - ${data.Email}</p>
+            <p>Phone - ${data.Phone}</p>
+            <p>Message - ${data.Message}</p>
+            
+            <p>Thanks,</p>
+             `,
+        attachments: [
+          {
+            filename: files.Resume.name,
+            content: files.Resume,
+          },
+        ],
+      });
+    } else {
+      entity = await strapi.services[controllerName].create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models[controllerName] });
+  },
+};
