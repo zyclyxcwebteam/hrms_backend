ac3b4dc21e74de321fbc8f65c5fe2ef5176a738d
skanjarla <skanjarla@zyclyx.com>
2020-10-23 18:23:09 +0530

    Update production URL

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://admin-zyclyx.herokuapp.com/",
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "57fc184bee8d6470793757761dffc185"),
