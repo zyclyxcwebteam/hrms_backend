e6d0073856e4f4a15fdfb498d465409014d87390
skanjarla <skanjarla@zyclyx.com>
2020-10-30 11:25:09 +0530

    Add blogposts and update collections

--- api/employee-profile/models/employee-profile.js
+++ api/employee-profile/models/employee-profile.js
@@ -1,8 +1,17 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+"use strict";
+module.exports = {
+  /**
+   * get Username and use it to update Employee_Name
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      // get user data here - username to use it for Employee_Name
+      const userId = data.user;
+      const user = await strapi
+        .query("user", "users-permissions")
+        .findOne({ id: userId });
+      // Update Employee Name in table entry
+      data.Employee_Name = user.username;
+    },
+  },
+};
