581eed48e0175b3de636460680908aedffc7f89a
skanjarla <skanjarla@zyclyx.com>
2020-11-11 11:59:54 +0530

    Update side nav link style

--- admin/src/components/LeftMenu/LeftMenuLink/LeftMenuLinkContent.js
+++ admin/src/components/LeftMenu/LeftMenuLink/LeftMenuLinkContent.js
@@ -4,29 +4,29 @@
  *
  */
 
-import React from 'react';
-import { startsWith } from 'lodash';
-import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import styled from 'styled-components';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import { startsWith } from "lodash";
+import PropTypes from "prop-types";
+import { FormattedMessage } from "react-intl";
+import styled from "styled-components";
+import { Link, withRouter } from "react-router-dom";
 
-import en from '../../../translations/en.json';
-import LeftMenuIcon from './LeftMenuIcon';
-import A from './A';
+import en from "../../../translations/en.json";
+import LeftMenuIcon from "./LeftMenuIcon";
+import A from "./A";
 
 const LinkLabel = styled.span`
   display: inline-block;
   width: 100%;
-  padding-right: 1rem;
+  padding-right: 0rem;
   padding-left: 2.5rem;
 `;
 
 // TODO: refacto this file
 const LeftMenuLinkContent = ({ destination, iconName, label, location }) => {
   const isLinkActive = startsWith(
-    location.pathname.replace('/admin', '').concat('/'),
-    destination.concat('/')
+    location.pathname.replace("/admin", "").concat("/"),
+    destination.concat("/")
   );
 
   // Check if messageId exists in en locale to prevent warning messages
@@ -35,21 +35,21 @@ const LeftMenuLinkContent = ({ destination, iconName, label, location }) => {
     en[labelId] || label.defaultMessage ? (
       <FormattedMessage
         id={labelId}
-        defaultMessage={label.defaultMessage || '{label}'}
+        defaultMessage={label.defaultMessage || "{label}"}
         values={{
           label: `${label.id || label}`,
         }}
       >
-        {message => <LinkLabel>{message}</LinkLabel>}
+        {(message) => <LinkLabel>{message}</LinkLabel>}
       </FormattedMessage>
     ) : (
       <LinkLabel>{labelId}</LinkLabel>
     );
 
   // Create external or internal link.
-  return destination.includes('http') ? (
+  return destination.includes("http") ? (
     <A
-      className={isLinkActive ? 'linkActive' : ''}
+      className={isLinkActive ? "linkActive" : ""}
       href={destination}
       target="_blank"
       rel="noopener noreferrer"
@@ -60,7 +60,7 @@ const LeftMenuLinkContent = ({ destination, iconName, label, location }) => {
   ) : (
     <A
       as={Link}
-      className={isLinkActive ? 'linkActive' : ''}
+      className={isLinkActive ? "linkActive" : ""}
       to={{
         pathname: destination,
       }}
--- admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
+++ admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const LeftMenuListLink = styled.div`
   margin-bottom: 19px;
-  margin-right: 28px;
+  margin-right: 0px;
   overflow: auto;
 `;
 
