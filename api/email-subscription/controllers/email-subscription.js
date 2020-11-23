0d2e529e97236cde2f3da1cc1f590bc6a8bf2d80
skanjarla <skanjarla@zyclyx.com>
2020-11-23 12:48:49 +0530

    Add email subscription

--- api/email-subscription/controllers/email-subscription.js
+++ api/email-subscription/controllers/email-subscription.js
@@ -1,8 +1,39 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    const controllerName = "email-subscription";
+    // query email to and cc list from emails-lists collection for careers_form
+    const emailList = await strapi
+      .query("email-lists")
+      .find({ List_Name: "contact_form" });
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services[controllerName].create(data, { files });
+    } else {
+      entity = await strapi.services[controllerName].create(ctx.request.body);
+    }
+
+    await strapi.plugins["email"].services.email.send({
+      to: emailList[0].To_List,
+      from: "hr.operations@zyclyx.com",
+      cc: emailList[0].CC_List,
+      replyTo: "hr.operations@zyclyx.com",
+      subject: `Newsletter Email Subscription`,
+      html: `
+          <p>Dear Team,</p>
+          <p>Please find the email address from news letter subscription form.</p>          
+          <p>Email - ${ctx.request.body.Subscriber_Email}</p>                    
+          <p>Thanks,</p>`,
+    });
+    return sanitizeEntity(entity, { model: strapi.models[controllerName] });
+  },
+};
